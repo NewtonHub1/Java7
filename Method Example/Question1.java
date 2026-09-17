15afb28e0a045b4fc3345606bac4f1919298e5bb
Newtonhub1 <nezsan072@gmail.com>
2026-09-17 15:04:23 +0545

    New Questions

---
+++ Method Example/Question1.java
@@ -0,0 +1,18 @@
+class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(){
+        this.id = 7;
+        this.name = "Newton" ;
+        this.age =16;
+    }
+    
+}
+
+
+public class Question1 {
+
+    
+}
